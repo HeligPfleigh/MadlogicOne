05fef55582a1d4515d80049c4853f208cbb1ae22
goldenhands92 <trantuan1892@gmail.com>
2020-07-27 13:32:23 +0700

    feat: add react native html render to render html string in native component like

--- src/screens/About.tsx
+++ src/screens/About.tsx
@@ -1,12 +1,25 @@
 import React from 'react';
-import {WebView} from 'react-native-webview';
 import {observer} from 'mobx-react-lite';
+import HTML from 'react-native-render-html';
+import {ScrollView, Dimensions} from 'react-native';
+import {useTheme} from 'react-native-paper';
+
 import {useStores} from '../core/hooks/useStores';
+import {useGlobalStyles} from '../core/hooks/useGlobalStyle';
 
 function About() {
   const store = useStores();
+  const theme = useTheme();
+  const [globalStyles] = useGlobalStyles(theme);
 
-  return <WebView source={{html: store?.ternantStore.about || '<div/>'}} />;
+  return (
+    <ScrollView style={globalStyles.container}>
+      <HTML
+        html={store?.ternantStore.about || '<div />'}
+        imagesMaxWidth={Dimensions.get('window').width}
+      />
+    </ScrollView>
+  );
 }
 
 export default observer(About);
